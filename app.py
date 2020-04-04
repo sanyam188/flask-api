b54a8eea7d9e8a23bcc55454fab57244d810515b
sanyam188 <sanyamjain.188@gmail.com>
2020-04-04 12:19:36 +0530

    data in api is changed

--- app.py
+++ app.py
@@ -6,8 +6,23 @@ def hello():
     # return 'Hello, Sanyam'
     return render_template('api.html')
 
-data=[{'name' : 'Sanyam','contact': '9690459433'
-},]
+data=[
+    {'id': 0,
+     'title': 'A Fire Upon the Deep',
+     'author': 'Vernor Vinge',
+     'first_sentence': 'The coldsleep itself was dreamless.',
+     'year_published': '1992'},
+    {'id': 1,
+     'title': 'The Ones Who Walk Away From Omelas',
+     'author': 'Ursula K. Le Guin',
+     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
+     'published': '1973'},
+    {'id': 2,
+     'title': 'Dhalgren',
+     'author': 'Samuel R. Delany',
+     'first_sentence': 'to wound the autumnal city.',
+     'published': '1975'}
+]
 
 @app.route('/<int:user>')
 def hello1(user):
