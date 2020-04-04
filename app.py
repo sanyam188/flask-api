f5d903d896dc4ad2794b4afb97871404a9bd7ec3
sanyam188 <sanyamjain.188@gmail.com>
2020-04-04 12:16:57 +0530

    Template created

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask
+from flask import Flask,render_template,jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello Sanyam '
+    # return 'Hello, Sanyam'
+    return render_template('api.html')
+
+data=[{'name' : 'Sanyam','contact': '9690459433'
+},]
 
 @app.route('/<int:user>')
 def hello1(user):
     return 'User %s' % user 
+
+@app.route('/link/')
+def link():
+    return jsonify(data)
 
 if __name__=='__main__':
     app.run(debug=True)
